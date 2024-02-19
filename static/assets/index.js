6641d2a92ac1f6d598367a113ac2daad7ea18fe1
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2024-02-19 01:49:00 +0100

    feat: implemented mark as complete & delete

--- static/assets/index.js
+++ static/assets/index.js
@@ -2,32 +2,93 @@ const ui = {
   inputElement: document.getElementById("title"),
   addButtonElement: document.getElementById("add-btn"),
   tableBody: document.querySelector("tbody"),
+  formElement: document.querySelector("form"),
 };
+
+const serverURL = "http://localhost:5000/todos";
 
 // function to fetch todos
 async function fetchTodos() {
-  const todos = await fetch("http://localhost:5000/todos");
+  const todos = await fetch(serverURL);
 
   return await todos.json();
+}
+
+// function to create todo
+async function createTodo(title) {
+  try {
+    const response = await fetch(serverURL, {
+      method: "POST",
+      body: JSON.stringify({ title }),
+    });
+
+    const todo = await response.json();
+
+    createTodoUi(todo);
+  } catch {
+    console.error("Something went wrong!");
+  }
 }
 
 // init function for app
 (async function () {
   const todos = await fetchTodos();
 
-  todos.forEach(({ id, title, complete }) => {
-    const [tableRow, todoID, todoTitle, todoStatus] = [
-      document.createElement("tr"),
-      document.createElement("td"),
-      document.createElement("td"),
-      document.createElement("td"),
-    ];
-
-    todoID.innerHTML = id;
-    todoTitle.innerHTML = title;
-    todoStatus.innerHTML = complete ? "Complete" : "Pending";
-
-    tableRow.append(...[todoID, todoTitle, todoStatus]);
-    ui.tableBody.append(tableRow);
-  });
+  todos.forEach((todo) => createTodoUi(todo));
 })();
+
+ui.formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createTodo(ui.inputElement.value);
+});
+
+function createTodoUi({ id, title, complete }) {
+  let [
+    tableRow,
+    todoID,
+    todoTitle,
+    todoStatus,
+    todoActions,
+    markAsCompleteBtn,
+    deleteBtn,
+  ] = [
+    document.createElement("tr"),
+    document.createElement("td"),
+    document.createElement("td"),
+    document.createElement("td"),
+    document.createElement("td"),
+    document.createElement("button"),
+    document.createElement("button"),
+  ];
+
+  // setting the data
+  todoID.innerHTML = id;
+  todoTitle.innerHTML = title;
+  todoStatus.innerHTML = complete ? "Complete" : "Pending";
+
+  // todo actions
+  markAsCompleteBtn.innerHTML = "✅";
+  deleteBtn.innerHTML = "❌";
+
+  // adding ids for styling
+  markAsCompleteBtn.id = "check-btn";
+  deleteBtn.id = "del-btn";
+
+  // adding event listener to each btn
+  markAsCompleteBtn.addEventListener("click", () => {
+    todoTitle.style.textDecoration = "line-through";
+    todoStatus.innerHTML = "Complete";
+  });
+
+  deleteBtn.addEventListener("click", () => {
+    // set all the feilds to null when item is deleted
+    [todoActions, todoTitle, todoStatus, todoID] = [null, null, null, null];
+    console.log([todoActions, todoTitle, todoStatus, todoID]);
+  });
+
+  // appending the btns to the actions row
+  todoActions.append(...[markAsCompleteBtn, deleteBtn]);
+
+  tableRow.append(...[todoID, todoTitle, todoStatus, todoActions]);
+  ui.tableBody.append(tableRow);
+}
